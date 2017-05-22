e3afa357d987b4d94dd2ba57948efe4942f5dc90
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-05-22 15:13:10 +0300

    ng 3.1.0 maintenance

--- web/main.dart
+++ web/main.dart
@@ -2,10 +2,11 @@ import 'dart:core';
 
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
-import 'package:angular2/src/core/reflection/reflection.dart';
 import 'package:angular2/router.dart';
 import 'package:angular2/platform/common.dart';
 
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import 'package:master_layout/master_layout_component.dart';
 import 'package:config/config_service.dart';
 import 'logger_component.dart';
@@ -24,23 +25,20 @@ bool get isDebug =>
 class AppComponent {}
 
 main() async {
-  if (isDebug) {
-    reflector.trackUsage();
-  }
-
   ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(MasterLayoutComponent),
     const Provider(AuthenticationService),
+    const Provider(AuthorizationService),
     const Provider(AlertService),
     const Provider(AsideService),
-    const Provider(ConfigService)
+    const Provider(ConfigService),
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
   ]);
 
   if (isDebug) {
     print('Application in DebugMode');
     enableDebugTools(ref);
-    print('Unused keys: ${reflector.listUnusedKeys()}');
   }
 }
